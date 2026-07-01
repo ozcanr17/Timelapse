136bdf250e24d92cc5bc1c8e909488e4e8ae520f
Rıdvan Özcan <ridvanozcan@yahoo.com>
2026-07-01 20:51:04 +0300

    Initial Commit

---
+++ Timelapse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Timelapse
+//
+//  Created by Rıdvan Özcan on 1.07.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Timelapse/TimelapseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimelapseApp.swift
+//  Timelapse
+//
+//  Created by Rıdvan Özcan on 1.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct TimelapseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
